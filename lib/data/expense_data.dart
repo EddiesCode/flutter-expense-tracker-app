11dfd405aba4925422ba9b3a2e242cf9ae36179f
Edward Karlsson <edwkar@student.chalmers.se>
2023-04-21 10:13:49 +0200

    added folders models and data, plus made the ExpenseItem class and ExpenseData class

---
+++ lib/data/expense_data.dart
@@ -0,0 +1,53 @@
+class ExpenseData {
+  // list ALL expenses
+
+  // get expense list
+
+  // add new expense
+
+  // delete expense
+
+  // get weekday (mon, tues, etc) from a dateTime object
+
+  // get the date of the start of the week ( sunday )
+
+  //
+
+  /* 
+  
+    convert overall list of expenses to daily expense summary
+
+    e.g.
+
+    overallExpanseList = 
+
+    [
+
+      [ food, 2023/01/30, $10 ],
+      [ hat, 2023/01/30, $15 ],
+      [ drinks, 2023/01/31, $1 ],
+      [ food, 2023/02/01, $5 ],
+      [ food, 2023/02/01, $6 ],
+      [ food, 2023/02/03, $7 ],
+      [ food, 2023/02/05, $10 ],
+      [ food, 2023/02/05, $11 ],
+
+
+    ]
+
+    ->
+
+    DailyExpenseSummary = 
+
+    [
+
+      [ 2023/01/30, $25 ],
+      [ 2023/01/31, $1 ],
+      [ 2023/02/01, $11 ],
+      [ 2023/02/03, $7 ],
+      [ 2023/02/05, $21 ],
+
+    ]
+  
+   */
+}
